20038fe2686fc16bce51813b23d2ad2c84725518
digvijaysinghkhatkar <digvijaykhatkar2005@gmail.com>
2025-01-08 18:56:32 +0530

    Delete task 1 directory

--- task 1/script.js
+++
@@ -1,9 +0,0 @@
-// Navbar background change on scroll
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-      navbar.style.backgroundColor = "#666";
-    } else {
-      navbar.style.backgroundColor = "#111";
-    }
-  });
